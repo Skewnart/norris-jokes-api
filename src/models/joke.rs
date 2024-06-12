6fb3ea4e61de8694b505d97fd5dc1388505c1764
Skewnart <corentinz@outlook.fr>
2024-06-13 00:09:16 +0200

    adding model joke (WIP)

---
+++ src/models/joke.rs
@@ -0,0 +1,24 @@
+use std::fmt; 
+// use serde::{Deserialize, Serialize};
+
+// #[derive(Clone, Serialize, Deserialize, Debug)] 
+#[derive(Clone, Debug)] 
+pub struct Joke {
+    pub id: String,
+    pub value: String
+}
+
+impl Joke {
+    pub fn new(id: &str, value: &str) -> Self {
+        Self {
+            id: id.to_string(),
+            value: value.to_string()
+        }
+    }
+}
+
+impl fmt::Display for Joke { 
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { 
+        write!(f, "{:?}", self) 
+    } 
+}
