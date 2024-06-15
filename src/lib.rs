33181e5208b8ff2f61541ef23d3779990d9e12ed
Skewnart <corentinz@outlook.fr>
2024-06-16 00:09:24 +0200

    Adding some genericity

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,16 @@
 mod joke;
 mod jokecategory;
 mod norriserror;
+mod request;
 
+use request::RequestClient;
 use joke::Joke;
 use norriserror::NorrisError;
-use reqwest::StatusCode;
 
-pub fn get_random() -> Result<Joke, NorrisError> {    
-    let response = reqwest::blocking::get("https://api.chucknorris.io/jokes/random").unwrap();
-
-    if response.status() != StatusCode::OK {
-        return Err(NorrisError::Generic(format!("{} : {}", response.url().path(), response.status())));
-    }
-
-    let content: Result<String, reqwest::Error> = response.text();
-    // println!("content: {:?}", content);
-
-    let json_text: String = match content {
-        Ok(text) => text,
-        Err(err) => return Err(NorrisError::Request(err))
-    };
-    // println!("json_text: {:?}", json_text);
-
-    let joke: Result<Joke, serde_json::Error> = serde_json::from_str(json_text.as_str());
-    // println!("joke: {:?}", joke);
-
-    match joke {
-        Ok(joke) => Ok(joke),
-        Err(err) => Err(NorrisError::Json(err))
-    }
+pub fn get_random() -> Result<Joke, NorrisError> {
+    RequestClient::new()
+        .retrieve_response_sync("/random")
+        .parse_it::<Joke>()
 }
 
 #[cfg(test)]
---
+++ src/request.rs
@@ -0,0 +1,53 @@
+
+use reqwest::{blocking::Response, StatusCode};
+use serde::Deserialize;
+
+use crate::norriserror::NorrisError;
+
+pub struct RequestClient {
+    url: String,
+    response: Option<Response>
+}
+
+impl RequestClient {
+    pub fn new() -> Self {
+        Self {
+            url: "https://api.chucknorris.io/jokes".to_string(),
+            response: None
+        }
+    }
+    
+    pub fn retrieve_response_sync(mut self, path: &str) -> Self {
+        self.response = Some(reqwest::blocking::get(self.url.clone() + path).unwrap());
+        self
+    }
+
+    pub fn parse_it<T>(self) -> Result<T, NorrisError> 
+        where T: for<'a> Deserialize<'a>{
+        let response = match self.response {
+            Some(x) => x,
+            None => return Err(NorrisError::Generic("Reponse not loaded yet.".to_string()))
+        };
+
+        if response.status() != StatusCode::OK {
+            return Err(NorrisError::Generic(format!("{} : {}", response.url().path(), response.status())));
+        }
+    
+        let content: Result<String, reqwest::Error> = response.text();
+        // println!("content: {:?}", content);
+    
+        let json_text: String = match content {
+            Ok(text) => text,
+            Err(err) => return Err(NorrisError::Request(err))
+        };
+        // println!("json_text: {:?}", json_text);
+    
+        let joke: Result<T, serde_json::Error> = serde_json::from_str(json_text.as_str());
+        // println!("joke: {:?}", joke);
+    
+        match joke {
+            Ok(joke) => Ok(joke),
+            Err(err) => Err(NorrisError::Json(err))
+        }
+    }
+}
